db7aa14311b653e2c3b75da30867e143f883c620
GlazeMarieB <glazehb@gmail.com>
2020-04-29 11:09:54 -0700

    c

--- EMG_Logger/Form1.Designer.cs
+++ EMG_Logger/Form1.Designer.cs
@@ -155,7 +155,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(457, 39);
+            this.button1.Location = new System.Drawing.Point(497, 39);
             this.button1.Margin = new System.Windows.Forms.Padding(1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 57);
